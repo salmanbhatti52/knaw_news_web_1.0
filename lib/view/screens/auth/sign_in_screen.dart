4ec0ee0cc400e76f3630cc16000a7fb3abb3ff46
Muhammad Adnan Ameen <90311877+Dani4772@users.noreply.github.com>
2023-03-31 10:30:25 +0500

    update

--- lib/view/screens/auth/sign_in_screen.dart
+++ lib/view/screens/auth/sign_in_screen.dart
@@ -66,172 +66,178 @@ class _SignInScreenState extends State<SignInScreen> {
               padding: const EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
               child: Center(
                 child: Container(
-                  child: Column(
-                      children: [
+                  child: AutofillGroup(
+                    child: Column(
+                        children: [
 
-                      const SizedBox(height: Dimensions.PADDING_SIZE_EXTRA_LARGE),
-
-                      Container(
-                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07),
-                        alignment: Alignment.centerLeft,
-                          child: Text('${AppData().language!.hey}, \n${AppData().language!.loginNow}', style: openSansExtraBold.copyWith(fontSize: 25)),
-                      ),
-                      SizedBox(height: 25),
+                        const SizedBox(height: Dimensions.PADDING_SIZE_EXTRA_LARGE),
 
                         Container(
-                          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07,right: MediaQuery.of(context).size.width*0.3),
+                          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07),
                           alignment: Alignment.centerLeft,
-                          child: Text(
-                            AppData().language!.enterNameAndPass,
-                            style: openSansMedium.copyWith(color: textColor,fontSize: Dimensions.fontSizeDefault,),
-                          ),
+                            child: Text('${AppData().language!.hey}, \n${AppData().language!.loginNow}', style: openSansExtraBold.copyWith(fontSize: 25)),
                         ),
-                        SizedBox(height: 20),
+                        SizedBox(height: 25),
+
+                          Container(
+                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.07,right: MediaQuery.of(context).size.width*0.3),
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              AppData().language!.enterNameAndPass,
+                              style: openSansMedium.copyWith(color: textColor,fontSize: Dimensions.fontSizeDefault,),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.8,
+                          child: Column(children: [
+
+                            TextField(
+                              autofillHints: [AutofillHints.username],
+                              onChanged: (value)=> profile.userName=value,
+                              style: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall,color: textColor),
+                              keyboardType: TextInputType.text,
+                              cursorColor: Theme.of(context).primaryColor,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                focusColor: const Color(0XF7F7F7),
+                                hoverColor: const Color(0XF7F7F7),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
+                                  borderSide: const BorderSide(style: BorderStyle.none, width: 0),
+                                ),
+                                isDense: true,
+                                hintText: AppData().language!.userName,
+                                fillColor: Color(0XBBF0F0F0),
+                                hintStyle: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall),
+                                filled: true,
+
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
+                                  child: SvgPicture.asset(Images.user_name, height: 5, width: 5,color: Colors.black),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 10,),
+
+                            TextField(
+                              autofillHints: [AutofillHints.password],
+                              onChanged: (value)=> profile.userPassword=value,
+                              style: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall,color: textColor),
+                              keyboardType: TextInputType.text,
+                              cursorColor: Theme.of(context).primaryColor,
+                              focusNode: _passwordFocus,
+                              autofocus: false,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                focusColor: const Color(0XF7F7F7),
+                                hoverColor: const Color(0XF7F7F7),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
+                                  borderSide: const BorderSide(style: BorderStyle.none, width: 0),
+                                ),
+                                isDense: true,
+                                hintText: AppData().language!.password,
+                                fillColor: Color(0XBBF0F0F0),
+                                hintStyle: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall),
+                                filled: true,
+
+                                prefixIcon: Padding(
+                                  padding: EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
+                                  child: SvgPicture.asset(Images.lock, height: 10, width: 20,color: Colors.black),
+                                ),
+                              ),
+                            ),
+                            Row(
+                                children: [
+                              Expanded(
+                                child: ListTile(
+                                  minLeadingWidth: 0,
+                                  hoverColor: null,
+                                  focusColor: null,
+                                  selectedTileColor: null,
+                                  tileColor: null,
+                                  leading: Checkbox(
+                                    side: BorderSide(color: Colors.red,width: 2),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5)
+                                    ),
+                                    checkColor: Colors.white,
+                                    activeColor: Colors.red,
+                                    value: isChecked,
+                                    onChanged: (val) => setState(() {
+                                      isChecked=val!;
+                                      TextInput.finishAutofillContext();
+                                    }),
+                                  ),
+                                  title: Text(AppData().language!.rememberMe,style: openSansRegular.copyWith(fontSize:Dimensions.fontSizeDefault,color: textColor),),
+                                  contentPadding: EdgeInsets.zero,
+                                  dense: true,
+                                  horizontalTitleGap: 0,
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Get.to(() => ForgetPassScreen());
+                                },
+                                child: Text('${AppData().language!.forgotPassword}?',
+                                  style: openSansMedium.copyWith(fontSize: Dimensions.fontSizeDefault,color: Colors.red),
+                                ),
+                              ),
+                            ]),
+
+
+
+                          ]),
+                        ),
+                        SizedBox(height: 10),
+
+                        SizedBox(height: Dimensions.PADDING_SIZE_LARGE),
 
                       Container(
-                        width: MediaQuery.of(context).size.width*0.8,
-                        child: Column(children: [
-
-                          TextField(
-                            onChanged: (value)=> profile.userName=value,
-                            style: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall,color: textColor),
-                            keyboardType: TextInputType.text,
-                            cursorColor: Theme.of(context).primaryColor,
-                            autofocus: false,
-                            decoration: InputDecoration(
-                              focusColor: const Color(0XF7F7F7),
-                              hoverColor: const Color(0XF7F7F7),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
-                                borderSide: const BorderSide(style: BorderStyle.none, width: 0),
-                              ),
-                              isDense: true,
-                              hintText: AppData().language!.userName,
-                              fillColor: Color(0XBBF0F0F0),
-                              hintStyle: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall),
-                              filled: true,
-
-                              prefixIcon: Padding(
-                                padding: EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
-                                child: SvgPicture.asset(Images.user_name, height: 5, width: 5,color: Colors.black),
-                              ),
-                            ),
-                          ),
-
-                          SizedBox(height: 10,),
-
-                          TextField(
-                            onChanged: (value)=> profile.userPassword=value,
-                            style: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall,color: textColor),
-                            keyboardType: TextInputType.text,
-                            cursorColor: Theme.of(context).primaryColor,
-                            focusNode: _passwordFocus,
-                            autofocus: false,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              focusColor: const Color(0XF7F7F7),
-                              hoverColor: const Color(0XF7F7F7),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(Dimensions.RADIUS_SMALL),
-                                borderSide: const BorderSide(style: BorderStyle.none, width: 0),
-                              ),
-                              isDense: true,
-                              hintText: AppData().language!.password,
-                              fillColor: Color(0XBBF0F0F0),
-                              hintStyle: openSansRegular.copyWith(fontSize: Dimensions.fontSizeSmall),
-                              filled: true,
-
-                              prefixIcon: Padding(
-                                padding: EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
-                                child: SvgPicture.asset(Images.lock, height: 10, width: 20,color: Colors.black),
-                              ),
-                            ),
-                          ),
-                          Row(
-                              children: [
-                            Expanded(
-                              child: ListTile(
-                                minLeadingWidth: 0,
-                                hoverColor: null,
-                                focusColor: null,
-                                selectedTileColor: null,
-                                tileColor: null,
-                                leading: Checkbox(
-                                  side: BorderSide(color: Colors.red,width: 2),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(5)
-                                  ),
-                                  checkColor: Colors.white,
-                                  activeColor: Colors.red,
-                                  value: isChecked,
-                                  onChanged: (val) => setState(() {
-                                    isChecked=val!;
-                                  }),
-                                ),
-                                title: Text(AppData().language!.rememberMe,style: openSansRegular.copyWith(fontSize:Dimensions.fontSizeDefault,color: textColor),),
-                                contentPadding: EdgeInsets.zero,
-                                dense: true,
-                                horizontalTitleGap: 0,
-                              ),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Get.to(() => ForgetPassScreen());
-                              },
-                              child: Text('${AppData().language!.forgotPassword}?',
-                                style: openSansMedium.copyWith(fontSize: Dimensions.fontSizeDefault,color: Colors.red),
-                              ),
-                            ),
+                        height: 45,
+                        width: MediaQuery.of(context).size.width*0.7,
+                        child: TextButton(
+                          onPressed: (){
+                            showCustomSnackBar("Login",isError: false);
+                            print("Login");
+                            TextInput.finishAutofillContext();
+                            _login();
+                          },
+                          style: flatButtonStyle,
+                          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                            Text(AppData().language!.loginNow, textAlign: TextAlign.center, style: openSansBold.copyWith(
+                              color: textBtnColor,
+                              fontSize: Dimensions.fontSizeDefault,
+                            )),
                           ]),
-
-
-
-                        ]),
-                      ),
-                      SizedBox(height: 10),
-
-                      SizedBox(height: Dimensions.PADDING_SIZE_LARGE),
-
-                    Container(
-                      height: 45,
-                      width: MediaQuery.of(context).size.width*0.7,
-                      child: TextButton(
-                        onPressed: (){
-                          showCustomSnackBar("Login",isError: false);
-                          print("Login");
-                          _login();
-                        },
-                        style: flatButtonStyle,
-                        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                          Text(AppData().language!.loginNow, textAlign: TextAlign.center, style: openSansBold.copyWith(
-                            color: textBtnColor,
-                            fontSize: Dimensions.fontSizeDefault,
-                          )),
-                        ]),
-                      ),
-                    ),
-
-
-
-                      SizedBox(height: 15),
-
-
-                      SizedBox(height: 40),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          minimumSize: Size(1, 40),
                         ),
-                        onPressed: () {
-
-                          Get.to(() => SignUpScreen());
-                        },
-                        child: RichText(text: TextSpan(children: [
-                          TextSpan(text: AppData().language!.iAmNewUser, style: openSansExtraBold.copyWith(color: textColor,fontSize: Dimensions.fontSizeDefault)),
-                          TextSpan(text: AppData().language!.signUp, style: openSansExtraBold.copyWith(color: Colors.red,fontSize: Dimensions.fontSizeDefault,decoration: TextDecoration.underline,decorationThickness: 4)),
-                        ])),
                       ),
 
-                    ]),
+
+
+                        SizedBox(height: 15),
+
+
+                        SizedBox(height: 40),
+                        TextButton(
+                          style: TextButton.styleFrom(
+                            minimumSize: Size(1, 40),
+                          ),
+                          onPressed: () {
+
+                            Get.to(() => SignUpScreen());
+                          },
+                          child: RichText(text: TextSpan(children: [
+                            TextSpan(text: AppData().language!.iAmNewUser, style: openSansExtraBold.copyWith(color: textColor,fontSize: Dimensions.fontSizeDefault)),
+                            TextSpan(text: AppData().language!.signUp, style: openSansExtraBold.copyWith(color: Colors.red,fontSize: Dimensions.fontSizeDefault,decoration: TextDecoration.underline,decorationThickness: 4)),
+                          ])),
+                        ),
+
+                      ]),
+                  ),
                   ),
                 ),
               ),
